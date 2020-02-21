7a11ede19b98ada4020b5c54e1b374d84f3b1649
srinivasan V <srini@techmindsllc.com>
2020-02-21 15:36:12 +0530

    updated code

--- registrationtask.js
+++ registrationtask.js
@@ -1,103 +1,121 @@
-function formvalidation(){
-    
-        var x = document.getElementById("FirstName").value;
-        var y=document.getElementById("LastName").value;
-        var z=document.getElementById("PhoneNumber").value;
-        var a=document.getElementById("UserName").value;
-        var t=document.getElementById("Email").value;
-        var b=document.getElementById("Password").value;
-        var c=document.getElementById("confirmPassword").value;
-        if (x == "") {
-            console.log('hiiii', x);
-          alert("FirstName must be filled out");
-          return false;
+function formvalidation() {
+
+    var x = document.getElementById("FirstName").value;
+    var y = document.getElementById("LastName").value;
+    var z = document.getElementById("PhoneNumber").value;
+    var a = document.getElementById("UserName").value;
+    var t = document.getElementById("Email").value;
+    var b = document.getElementById("Password").value;
+    var c = document.getElementById("confirmPassword").value;
+    var dob = document.getElementById("dob").value;
+    var html = '<ul>';
+    if (x == "") {
+        console.log('hiiii', x);
+        html += "<li>FirstName must be filled out</li>";
+    }
+    if (y == "") {
+        html += "<li>LastName must be filled out</li>";
+    }
+    if (z == "") {
+        html += "<li>PhoneNumber must be filled out</li>";
+    }
+    if (a == "") {
+        html += "<li>UserName must be filled out</li>";
+    }
+    if (t == "") {
+        html += "<li>Email must be filled out</li>";
+    }
+    if (b == "") {
+        html += "<li>Password must be filled out</li>";
+    }
+    if (c == "") {
+        html += "<li>confirmPassword must be filled out</li>";
+    }
+    html += fName(html);
+    html += lName(html);
+    html += phonenumber(z, html);
+    html += uName(a, html);
+    html += pwdvalid(b, c, html);
+    html += checkEmail(html);
+    html += '</ul>';
+    document.getElementById('error-message').innerHTML = html;
+    let userobject = {};
+    if (html.trim() == '<ul></ul>') {
+        userobject["firstname"] = x;
+        userobject["lastname"] = y;
+        userobject["phone"] = z;
+        userobject["username"] = a;
+        userobject["email"] = t;
+        userobject["password"] = b;
+        userobject["dob"] = dob;
+        let userData = JSON.parse(localStorage.getItem("userData"));
+        if (userData && Array.isArray(userData)) {
+            userData.push(userobject)
+            localStorage.setItem("userData", JSON.stringify(userData))
         }
-        if (y == "") {
-            alert("LastName must be filled out");
-            return false;
+        else {
+            var userArray = [];
+            userArray.push(userobject)
+            localStorage.setItem("userData", JSON.stringify(userArray))
         }
-        if(z==""){
-            alert("PhoneNumber must be filled out");
-            return false;
-        }
-        if(a==""){
-            alert("UserName must be filled out");
-            return false;
-        }
-        if(t==""){
-            alert("Email must be filled out");
-            return false;
-        }
-        if(b==""){
-            alert("Password must be filled out");
-            return false;
-        }
-        if(c==""){
-            alert("confirmPassword must be filled out");
-            return false;
-        }
-      
-        fName();
-        lName();
-        phonenumber(z);
-        uName(a);
-        pwdvalid(b,c);
-        checkEmail();
+        document.getElementById('success-message').innerHTML = '<p>Successfully Registered!!!</p>';
+    }
 
 }
-function fName(){
+function fName() {
+    var html = '';
     var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-     y = document.getElementById('LastName').value;
-    if(!regName.test(y)){
-        alert('Invalid name given.');
+    y = document.getElementById('LastName').value;
+    if (!regName.test(y)) {
+        html += '<li>Invalid name given.</li>';
     }
+    return html;
 }
-function lName(){
+function lName() {
+    var html = '';
     var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
     var x = document.getElementById('FirstName').value;
-    if(!regName.test(x)){
-        alert('Invalid name given.');
+    if (!regName.test(x)) {
+        html += '<li>Invalid name given.</li>';
     }
+    return html;
 }
-function phonenumber(inputtxt)
-{
-  var phoneno = /^\d{10}$/;
-  var z = document.getElementById('PhoneNumber').value;
-  if(!phoneno.test(z)){
-    alert('phone number is invalid');
-}
-}
-function uName(k){
-var letters = /^[A-Za-z]+$/;
-if(letters.test(k))
-{
-return true;
-}
-else
-{
-alert('Username must have alphabet characters only');
-k.focus();
- return false;
- }
-}
-function pwdvalid(f1,f2){
-    alert(f1);
-    alert(f2);
-    if(f1 == f2){
-        alert("password matched");
+function phonenumber(inputtxt, ) {
+    var html = '';
+    var phoneno = /^\d{10}$/;
+    var z = document.getElementById('PhoneNumber').value;
+    if (!phoneno.test(z)) {
+        html += '<li>phone number is invalid</li>';
     }
-    else{
-        alert("password not matched");
+    return html;
+}
+function uName(k, ) {
+    var html = '';
+    var letters = /^[A-Za-z]+$/;
+    if (letters.test(k)) {
     }
+    else {
+        html += '<li>Username must have alphabet characters only</li>';
+    }
+    return html;
+}
+function pwdvalid(f1, f2, ) {
+    var html = '';
+    if (f1 == f2) {
+    }
+    else {
+        html += "<li>password not matched";
+    }
+    return html;
 }
 function checkEmail() {
-
-    var t =document.getElementById("Email");
+    var html = '';
+    var t = document.getElementById("Email");
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!filter.test(t.value)) {
-    alert('Please provide a valid email address');
-    t.focus;
-    return false;
- }
+        html += '<li>Please provide a valid email address<li>';
+        // t.focus;
+    }
+    return html;
 }
